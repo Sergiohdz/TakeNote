3a1935cd6f9b6269ff480332331b2268935250bf
bernease <bernease@gmail.com>
2013-01-19 21:14:15 -0500

    added command line args, better edges

--- detection.py
+++ detection.py
@@ -1,32 +1,48 @@
+import sys
 import cv2
+import random
 from math import pi
 from math import sqrt
 
-imagefiles = ["//home/bernease/Downloads/IMG_4364.jpg","//home/bernease/Downloads/IMG_4363.jpg",
-"//home/bernease/Downloads/IMG_4338.JPG","//home/bernease/Downloads/IMG_4315.JPG"]
+imagefiles = []
+
+for arg in sys.argv:
+    if arg != "detection.py":
+        imagefiles.append(arg)
 
 for imagefile in imagefiles:
+    # Read in image
     img = cv2.imread(imagefile)
-    imgcopy=img
-    #img = cv2.transpose(img)
+    # Convert to gray
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Find height and width
     h,w = img.shape[:2]
+    # Apply Gaussian Blur
     blur = cv2.GaussianBlur(gray, (9,9), 1)
-    #blur = cv2.GaussianBlur(blur, (9,9), 1.4)
-
+    # Apply Canny edge detection
     edges = cv2.Canny(blur, 0.4*255, 0.4*0.4*255, imgcopy, 3)
-  
+    # Apply Hough line detection
     lines = cv2.HoughLinesP(edges, 1, pi/720, 150, None, min(h,w)/10, min(h,w)/50);
+
+    # For later use in shape detection
+    thirdleft = w/3
+    thirdright = w*2/3
+    thirdup = h/3
+    thirddown = h*2/3
+    n = 10
+    xvalues = random.sample(thirdleft:thirdright,n)
+    yvalues = random.sample(thirdup:thirddown,n)
 
     for line in lines[0]:
         # Find points
         pt1 = (line[0],line[1])
         pt2 = (line[2],line[3])
-        # Find slope
         if line[2] == line[0]:
             pt1x = (line[0],0)
             pt2x = (line[0],h)
+            m = float("Inf")
         else:
+            # Find slope m
             m = float(line[3] - line[1]) / float(line[2] - line[0])
             # Solve for b, far left
             b = int(line[1] - m*line[0])
@@ -37,22 +53,13 @@ for imagefile in imagefiles:
             pt2x = (w,y)
 
         # Draw lines
-        #cv2.line(img, pt1x, pt2x, (0,0,127), 3)
-        cv2.line(img, pt1, pt2, (0,0,255), 3)
+        # cv2.line(img, pt1x, pt2x, (0,0,127), 3)  #extended
+        cv2.line(img, pt1, pt2, (0,0,255), 3)   #actual
 
-    # Write image (with lines) and edges to jpg    cvtColor( dst, color_dst, CV_GRAY2BGR );
+        # SHAPE DETECTION
 
-    outfile = imagefile[:-4] + "lines.jpg"
-    outfileedge = imagefile[:-4] + "edges.jpg"
+    # Write image (with lines) and edges to jpg
+    outfile = imagefile[:-4] + "_out.jpg"
+    #outfileedge = imagefile[:-4] + "edges.jpg"
     cv2.imwrite(outfile, img)
-    cv2.imwrite(outfileedge, edges)
-
-    #mser = cv2.MSER()
-    #regions = mser.detect(gray,None)
-    #maxr = 0
-    #for region in regions:
-    #    lenr = float(len(region))
-    #    if lenr > maxr:
-    #        maxr = lenr/(h*w)
-
-    #print maxr
+    #cv2.imwrite(outfileedge, edges)
